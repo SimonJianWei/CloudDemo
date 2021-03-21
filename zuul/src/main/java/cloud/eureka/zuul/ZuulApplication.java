618bcec91aaa30103fa5de1d4518cd7501dc86ac
SimonJianWei <simonjianwei@163.com>
2021-03-21 09:47:45 +0800

    提交

--- zuul/src/main/java/cloud/eureka/zuul/ZuulApplication.java
+++ zuul/src/main/java/cloud/eureka/zuul/ZuulApplication.java
@@ -2,8 +2,9 @@ package cloud.eureka.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ZuulApplication {
 
 	public static void main(String[] args) {
